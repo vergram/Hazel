686ac0a70b8cf147ba4acd89703cecc4a08624b3
Vvvvergram <1074034978@qq.com>
2018-11-05 13:23:48 +0800

    warp spdlog to Hazel::Log

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,6 +6,12 @@ extern Hazel::Application* CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Hazel Initialized!");
+	int a = 5;
+	HZ_INFO("var a = {0}", a);
+	::Hazel::Log::GetClientLogger()->error("test");
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
