c9820fdc2dee7cd490a6644af6be254444146f01
Vvvvergram <1074034978@qq.com>
2019-03-07 10:38:54 +0800

    actual define HZ_ASSERT in Core.h

--- Hazel/src/Hazel/Core.h
+++ Hazel/src/Hazel/Core.h
@@ -10,13 +10,16 @@
 	#error Hazel only support Windows!
 #endif // HZ_PLATFORM_WINDOWS
 
+#ifdef HZ_DEBUG
+	#define HZ_ENABLE_ASSERTS
+#endif // HZ_DEBUG
 
 #ifdef HZ_ENABLE_ASSERTS
-    #define HZ_ASSERTS(x, ...) {if(!(x)) {HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak();}}
-    #define HZ_CORE_ASSERTS(x, ...) {if(!(x)) {HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak();}}
+    #define HZ_ASSERT(x, ...) {if(!(x)) {HZ_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak();}}
+    #define HZ_CORE_ASSERT(x, ...) {if(!(x)) {HZ_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak();}}
 #else
-    #define HZ_ASSERTS(x, ...) 
-    #define HZ_CORE_ASSERTS(x, ...)
+    #define HZ_ASSERT(x, ...) 
+    #define HZ_CORE_ASSERT(x, ...)
 #endif // HZ_ENABLE_ASSERTS
 
 
