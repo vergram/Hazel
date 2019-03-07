f8595b78c91184a2806c09e412424c54b080a666
Vvvvergram <1074034978@qq.com>
2019-03-07 14:55:32 +0800

    added mousecode and keycode from glfw3.h

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,8 +3,12 @@
 // For use by Hazel applications
 
 #include "Hazel/Application.h"
-#include "Hazel/Log.h"
 #include "Hazel/Layer.h"
+#include "Hazel/Log.h"
+
+#include "Hazel/Input.h"
+#include "Hazel/KeyCodes.h"
+#include "Hazel/MouseCodes.h"
 
 #include "Hazel/ImGui/ImGuiLayer.h"
 
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -7,12 +7,20 @@ public:
 
 	void OnUpdate() override
 	{
-		//HZ_TRACE("Example Layer On Update");
+		if (Hazel::Input::IsKeyPressed(HZ_KEY_A))
+		{
+			HZ_TRACE("Polling Key: {0}", (char)HZ_KEY_A);
+		}
 	}
 
 	void OnEvent(Hazel::Event& event) override
 	{
-		//HZ_TRACE("{0}", event);
+		if (event.GetEventType() == Hazel::EventType::KeyPressed)
+		{
+			Hazel::KeyPressedEvent& e = (Hazel::KeyPressedEvent&)event;
+			if(e.GetKeyCode() == HZ_KEY_A)
+				HZ_TRACE("Event Key: {0}", (char)e.GetKeyCode());
+		}
 	}
 };
 
